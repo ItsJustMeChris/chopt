4601bbfb6d93bafa8b7a00f9efe25d2cf8fd166a
Chris McGravey <chris@mcgravey.dev>
2025-11-19 12:10:06 -0600

    Initial

---
+++ src/client/java/mod/chopt/ChoptClient.java
@@ -0,0 +1,10 @@
+package mod.chopt;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ChoptClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
